f4a2c965f82db0dd1b9cec559e9357f3c0fd3a1b
Michal <michal.gwozdz123@gmail.com>
2022-03-15 13:00:15 +0100

    update .gitignore, remove comments, add println

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,27 +1,10 @@
 package pl.lublin.wsei.java.cwiczenia;
 
-/**
- *
- * @author Mich
- * @serial
- *
- */
-
-
-
 public class Main {
 
     public static void main(String[] args) {
 
-        // komentarz jednolinijkowy
-
-
-        /*
-        komentarz
-
-        wielolinijkowy
-         */
-
+   System.out.println(" Hello java world\n ");
 
     }
 }
